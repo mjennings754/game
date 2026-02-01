725f6db90c508af8b8133878edfd8953ee98571c
mjennings754 <mitchellj@outlook.jp>
2026-02-01 22:11:18 +1100

    remove print(screen_scroll)

--- main.py
+++ main.py
@@ -142,8 +142,6 @@ while running:
     player.update()
     player.draw()
     player.move(moving_left, moving_right)
-
-    print(screen_scroll)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
